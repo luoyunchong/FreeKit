122350b834354a6cb7b763178b4626a11806c3d6
igeekfan <luoyunchong@foxmail.com>
2022-06-28 02:13:03 +0800

    :sparkles: Add FreeRedis+DataProtection

---
+++ src/IGeekFan.AspNetCore.DataProtection.FreeRedis/FreeRedisDataProtectionBuilderExtensions.cs
@@ -0,0 +1,78 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using FreeRedis;
+using IGeekFan.AspNetCore.DataProtection.FreeRedis;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.DataProtection;
+
+/// <summary>
+/// Contains Redis-specific extension methods for modifying a <see cref="IDataProtectionBuilder"/>.
+/// </summary>
+public static class FreeRedisDataProtectionBuilderExtensions
+{
+    private const string DataProtectionKeysName = "DataProtection-Keys";
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to specified key in Redis database
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="databaseFactory">The delegate used to create <see cref="IRedisClient"/> instances.</param>
+    /// <param name="key">The key used to store key list.</param>
+    /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+    public static IDataProtectionBuilder PersistKeysToStackExchangeRedis(this IDataProtectionBuilder builder, Func<IRedisClient> databaseFactory, string key)
+    {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+        if (databaseFactory == null)
+        {
+            throw new ArgumentNullException(nameof(databaseFactory));
+        }
+        return PersistKeysToStackExchangeRedisInternal(builder, databaseFactory, key);
+    }
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to the default key ('DataProtection-Keys') in Redis database
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="connectionMultiplexer">The <see cref="IRedisClient"/> for database access.</param>
+    /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+    public static IDataProtectionBuilder PersistKeysToStackExchangeRedis(this IDataProtectionBuilder builder, IRedisClient connectionMultiplexer)
+    {
+        return PersistKeysToStackExchangeRedis(builder, connectionMultiplexer, DataProtectionKeysName);
+    }
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to the specified key in Redis database
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="connectionMultiplexer">The <see cref="IRedisClient"/> for database access.</param>
+    /// <param name="key">The key used to store key list.</param>
+    /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+    public static IDataProtectionBuilder PersistKeysToStackExchangeRedis(this IDataProtectionBuilder builder, IRedisClient connectionMultiplexer, string key)
+    {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+        if (connectionMultiplexer == null)
+        {
+            throw new ArgumentNullException(nameof(connectionMultiplexer));
+        }
+        return PersistKeysToStackExchangeRedisInternal(builder, () => connectionMultiplexer.GetDatabase(), key);
+    }
+
+    private static IDataProtectionBuilder PersistKeysToStackExchangeRedisInternal(IDataProtectionBuilder builder, Func<IRedisClient> databaseFactory, string key)
+    {
+        builder.Services.Configure<KeyManagementOptions>(options =>
+        {
+            options.XmlRepository = new RedisXmlRepository(databaseFactory, key);
+        });
+        return builder;
+    }
+}
---
+++ src/IGeekFan.AspNetCore.DataProtection.FreeRedis/RedisXmlRepository.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using FreeRedis;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+
+namespace IGeekFan.AspNetCore.DataProtection.FreeRedis;
+
+/// <summary>
+/// An XML repository backed by a Redis list entry.
+/// </summary>
+public class RedisXmlRepository : IXmlRepository
+{
+    private readonly Func<IRedisClient> _databaseFactory;
+    private readonly string _key;
+
+    /// <summary>
+    /// Creates a <see cref="RedisXmlRepository"/> with keys stored at the given directory.
+    /// </summary>
+    /// <param name="databaseFactory">The delegate used to create <see cref="IRedisClient"/> instances.</param>
+    /// <param name="key">The key used to store key list.</param>
+    public RedisXmlRepository(Func<IRedisClient> databaseFactory, string key)
+    {
+        _databaseFactory = databaseFactory;
+        _key = key;
+    }
+
+    /// <inheritdoc />
+    public IReadOnlyCollection<XElement> GetAllElements()
+    {
+        return GetAllElementsCore().ToList().AsReadOnly();
+    }
+
+    private IEnumerable<XElement> GetAllElementsCore()
+    {
+        // Note: Inability to read any value is considered a fatal error (since the file may contain
+        // revocation information), and we'll fail the entire operation rather than return a partial
+        // set of elements. If a value contains well-formed XML but its contents are meaningless, we
+        // won't fail that operation here. The caller is responsible for failing as appropriate given
+        // that scenario.
+        var database = _databaseFactory();
+        foreach (var value in database.LRange(_key, 0, -1))
+        {
+            yield return XElement.Parse(value);
+        }
+    }
+
+    /// <inheritdoc />
+    public void StoreElement(XElement element, string friendlyName)
+    {
+        var database = _databaseFactory();
+        database.RPush(_key, element.ToString(SaveOptions.DisableFormatting));
+    }
+}
