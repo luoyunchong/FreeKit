52968567645873ad66c5b01004de6eeb400e7e60
igeekfan <igeekfan@foxmail.com>
2024-03-09 16:07:24 +0800

    Modified the `UseConnectionString` method in `FreeSqlExtension.cs` to include an optional `prefix` parameter

--- src/IGeekFan.FreeKit.Extras/FreeSql/FreeSqlExtension.cs
+++ src/IGeekFan.FreeKit.Extras/FreeSql/FreeSqlExtension.cs
@@ -20,10 +20,10 @@ namespace FreeSql
         /// <param name="this"></param>
         /// <param name="configuration"></param>
         /// <returns></returns>
-        public static FreeSqlBuilder UseConnectionString(this FreeSqlBuilder @this, IConfiguration configuration)
+        public static FreeSqlBuilder UseConnectionString(this FreeSqlBuilder @this, IConfiguration configuration, string prefix = "ConnectionStrings")
         {
-            IConfigurationSection dbTypeCode = configuration.GetSection("ConnectionStrings:DefaultDB");
-            IConfigurationSection providerTypeSection = configuration.GetSection("ConnectionStrings:ProviderType");
+            IConfigurationSection dbTypeCode = configuration.GetSection($"{prefix}:DefaultDB");
+            IConfigurationSection providerTypeSection = configuration.GetSection($"{prefix}:ProviderType");
             var providerType = providerTypeSection.Value.IsNotNullOrWhiteSpace()
                 ? Type.GetType(providerTypeSection.Value)
                 : null;
@@ -32,16 +32,16 @@ namespace FreeSql
             {
                 if (!Enum.IsDefined(typeof(DataType), dataType))
                 {
-                    Trace.WriteLine($"数据库配置ConnectionStrings:DefaultDB:{dataType}无效");
+                    Trace.WriteLine($"数据库配置{{prefixs:DefaultDB:{dataType}无效");
                 }
 
                 IConfigurationSection connectionStringSection =
-                    configuration.GetSection($"ConnectionStrings:{dataType}");
+                    configuration.GetSection($"{prefix}:{dataType}");
                 @this.UseConnectionString(dataType, connectionStringSection.Value, providerType);
             }
             else
             {
-                Trace.WriteLine($"数据库配置ConnectionStrings:DefaultDB:{dbTypeCode.Value}无效");
+                Trace.WriteLine($"数据库配置{prefix}:DefaultDB:{dbTypeCode.Value}无效");
             }
 
             return @this;
---
+++ test/IGeekFan.FreeKit.xUnit/Extras/FreeSql/FreeSqlExtensionTest.cs
@@ -0,0 +1,29 @@
+﻿using FreeSql;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IGeekFan.FreeKit.xUnit.Extras.FreeSql
+{
+    public class FreeSqlExtensionTest
+    {
+        private readonly IConfiguration configuration;
+
+        public FreeSqlExtensionTest(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [Fact]
+        public void UseConnectionStringTest()
+        {
+
+            var fsql1 = new FreeSqlBuilder().UseConnectionString(configuration);
+            var fsql2 = new FreeSqlBuilder().UseConnectionString(configuration, "CsLog");
+        }
+    }
+}
